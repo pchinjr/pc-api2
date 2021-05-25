183b4efe32ef9c2241c123f5c0495d8d60bc6e25
Paul Chin Jr <pchinjr@gmail.com>
2021-05-25 13:39:22 -0400

    api route

---
+++ api/index.js
@@ -0,0 +1,11 @@
+const arc = require('@architect/functions')
+
+exports.handler = arc.http.async(route)
+
+async function route(req) {
+  return {
+    statusCode: 200,
+    json:
+      { image: `${process.env.BEGIN_STATIC_ORIGIN}/1.jpeg` }
+  }
+}
